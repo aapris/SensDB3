40b2f7ed21546d6d4b48644842226ad57fdf1f21
Aapo Rista <aapo.gitpub@rista.net>
2017-07-10 18:23:58 +0300

    Add very simple endpoint for custom "espeasy" protocol

--- sensdb_api/urls.py
+++ sensdb_api/urls.py
@@ -1,10 +1,12 @@
 """
 Include all API versions, which are in use.
+Also define all non-REST endpoints here.
 """
 
-from django.conf import settings
 from django.conf.urls import url, include
+from . import views
 
 urlpatterns = [
     url(r'^v1/', include('sensdb_api.v1.urls', namespace='v1')),
+    url(r'^espeasy/?$', views.postdata_espeasy, name='postdata_espeasy'),
 ]
--- sensdb_api/views.py
+++ sensdb_api/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+import base64
+import json
+import datetime
+from django.contrib.auth import authenticate
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from sensdb3.models import Datapost
+from sensdb_api.tasks import process_dataposts_task
 
-# Create your views here.
+
+def _basicauth(request):
+    # Check for valid basic auth header
+    uname, passwd, user = None, None, None
+    if 'HTTP_AUTHORIZATION' in request.META:
+        auth = request.META['HTTP_AUTHORIZATION'].split()
+        if len(auth) == 2:
+            if auth[0].lower() == "basic":
+                a = auth[1].encode('utf8')
+                s = base64.b64decode(a)
+                uname, passwd = s.decode('utf8').split(':')
+                user = authenticate(username=uname, password=passwd)
+    return uname, passwd, user
+
+
+@csrf_exempt
+def postdata_espeasy(request, version='0.0.0'):
+    """
+    POST data using self made "espeasy" protocol. Example below uses Httpie application.
+    echo -n "idcode=logger_id_code&sensor=bme280&id=0&data=Temperature=24.84,Humidity=52.05,Pressure=1002.50" |
+       http -v --auth user:pass --form POST http://127.0.0.1:8000/api/espeasy
+    """
+    uname, passwd, user = _basicauth(request)
+    # You might want to return HTTP 403 here, if user was not authenticated
+    data = request.POST.get('data', '').strip()
+    # lograw.info(data)
+    dp = None
+    if data:
+        json_data = json.dumps(request.POST)
+        idcode = request.POST.get('idcode', '').strip()
+        # sensor = request.POST.get('sensor', '').strip()
+        idcode = idcode.replace('\r', '').replace('\n', '')
+        dp = Datapost(data=json_data, idcode=idcode)
+        if idcode == '':
+            pass  # TODO: set dp.status = 'NO_IDCODE' ?
+        dp.protocol = request.POST.get('protocol', 'ESPEASY').strip()
+        dp.version = request.POST.get('version', version).strip()
+        dp.user = user
+        dp.set_request_data(request)
+        dp.save()
+        try:
+            process_dataposts_task.delay(dp.pk)
+        except Exception as err:  # Broker is not listening: redis.exceptions.ConnectionError
+            # lograw.info(err)
+            print(type(err))
+    utc_dt = timezone.now()
+    time_str = utc_dt.strftime('%Y-%m-%dT%H:%M:%SZ')
+    responsetext = '$OK,{}'.format(time_str)
+    if dp:
+        dp.response = responsetext
+        dp.save()
+    response = HttpResponse(responsetext)
+    return response
