1e7cf749bfccb4d857bc78851a626e4ec73d27ff
Aapo Rista <aapo.gitpub@rista.net>
2017-07-10 15:39:34 +0300

    Add modules to INSTALLED_APPS

--- sensdb3_project/sensdb3_project/settings.py
+++ sensdb3_project/sensdb3_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
+    'sensdb3',
+    'sensdb_api',
 ]
 
 MIDDLEWARE = [
