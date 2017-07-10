d3d600bd14c9a9a82ce48a2b2af14df79e2e231c
Aapo Rista <aapo.gitpub@rista.net>
2017-07-10 15:39:17 +0300

    Add app dirs to sys.path

--- sensdb3_project/manage.py
+++ sensdb3_project/manage.py
@@ -3,6 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.append(BASE_DIR)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sensdb3_project.settings")
     try:
         from django.core.management import execute_from_command_line
